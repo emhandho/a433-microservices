edc34d340bc6c60e2f30afba019ce982b586281a
emhandho <nurmanrdh@gmail.com>
2023-05-18 23:16:51 +0700

    add: comment and change some commands

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,8 +1,14 @@
 #! /bin/bash
 
-docker build -t item-app:v1 . \
-	&& docker images \
-	&& docker image item-app:v1 emhandho/item-app:v1 \
-	&& EXPORT PASSWORD_DOCKER_HUB="!QwertyriD@2008." \
-	&& echo $PASSWORD_DOCKER_HUB | docker login -u emhandho --password-stdin \
-	&& docker push emhandho/item-app:v1;
+# perintah untuk build docker image dengan nama image item-app dan tag v1
+docker build -t item-app:v1 . && \
+	# perintah untuk menampilkan list docker image di local repository
+	docker images && \
+	# perintah untuk mengubah nama image agar sesuai dengan format GitHub Packages
+	docker image tag item-app:v1 ghcr.io/emhandho/item-app:v1 && \
+	# perintah untuk menyimpan environment variable untuk merahasiakan access_token GitHub Packages
+	export CR_PAT="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" && \
+	# perintah untuk login ke GitHub Pakcages menggunakan access_token yang sudah dideklarasi pada perintah sebelumnya
+	echo $CR_PAT | docker login ghcr.io -u emhandho --password-stdin && \
+	# perintah untuk mengunggah docker image dari lokal ke GitHub Packages
+	docker push ghcr.io/emhandho/item-app:v1;
