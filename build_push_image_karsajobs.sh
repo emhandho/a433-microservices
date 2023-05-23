7e7cd9ce25fa0b91a472399a13ce4853e7e534ea
emhandho <nurmanrdh@gmail.com>
2023-05-23 21:21:36 +0700

    init build and push image script

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# perintah untuk build docker image dengan nama image item-app dan tag v1
+docker build -t ghcr.io/emhandho/karsajobs:latest . && \
+	# perintah untuk menyimpan environment variable untuk merahasiakan access_token GitHub Packages
+	export CR_PAT="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" && \
+	# perintah untuk login ke GitHub Pakcages menggunakan access_token yang sudah dideklarasi pada perintah sebelumnya
+	echo $CR_PAT | docker login ghcr.io -u emhandho --password-stdin && \
+	# perintah untuk mengunggah docker image dari lokal ke GitHub Packages
+	docker push ghcr.io/emhandho/karsajobs:latest;
