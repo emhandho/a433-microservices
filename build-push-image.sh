25347348b5b9415bf0d1b05699c0c835311ed694
emhandho <nurmanrd@gmail.com>
2023-06-26 16:38:08 +0700

    update project

--- build-push-image.sh
+++ build-push-image.sh
@@ -2,8 +2,6 @@
 
 # perintah untuk build docker image dengan nama image item-app dan tag v1
 docker build -t ghcr.io/emhandho/fp-shipping-service:latest . && \
-        # perintah untuk menyimpan environment variable untuk merahasiakan access_token GitHub Packages
-        export CR_PAT="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" && \
         # perintah untuk login ke GitHub Pakcages menggunakan access_token yang sudah dideklarasi pada perintah sebelumnya
         echo $CR_PAT | docker login ghcr.io -u emhandho --password-stdin && \
         # perintah untuk mengunggah docker image dari lokal ke GitHub Packages
